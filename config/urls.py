cc06303f0f2279b65d4c76be2fec42b463b55960
Panjiaphu <128072003+Panjiaphu@users.noreply.github.com>
2026-06-15 15:16:28 +0800

    chore: add Render health check route

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from .health import healthz
+
 
 urlpatterns = [
+    path("healthz/", healthz, name="healthz"),
     path("admin/", admin.site.urls),
     path("", include("odds.urls")),
 ]
